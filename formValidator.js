5493bfed217811dd938eb832bfae54cdad87b046
YounQsheyex <younqsheyex@gmail.com>
2025-08-07 05:44:39 -0700

    feat:update url regex

--- formValidator.js
+++ formValidator.js
@@ -2,9 +2,13 @@ import * as Yup from "yup";
 
 export const linkSchema = Yup.object().shape({
   webUrl: Yup.string()
+    .required("Please add a link")
     .matches(
-      /^(https?:\/\/)?(www\.)?[a-zA-Z0-9\-]+\.[a-zA-Z]{2,}(\/\S*)?$/,
+      /\b((https?:\/\/)?(www\.)?([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,})(:[0-9]+)?(\/[^\s]*)?\b/,
       "Please add a link"
-    )
-    .required("Please add a link"),
+    ),
 });
+
+//   /^(https?:\/\/)?(www\.)?[a-zA-Z0-9\-]+\.[a-zA-Z]{2,}(\/\S*)?$/,
+
+// \b((http[s]?://)?(www\.)?([a-zA-Z0-9\-]+\.)+[a-zA-Z]{2,})(:[0-9]+)?(/[^\s]*)?\b
